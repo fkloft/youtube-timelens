67417f687098868a92b3503766af174e436cad9d
Felix Kloft <felix.kloft@gmail.com>
2018-12-16 20:52:32 +0100

    increase opacity

--- youtube-timelens.user.js
+++ youtube-timelens.user.js
@@ -3,7 +3,7 @@
 // @description     Generates a timelens (see https://timelens.io/) from YouTube's storyboard thumbnails
 // @namespace       https://github.com/fkloft
 // @include         https://www.youtube.com/*
-// @version         1.0.1
+// @version         1.0.2
 // @grant           none
 // @run-at          document-end
 // ==/UserScript==
@@ -203,7 +203,7 @@ style.textContent = `
 }
 .ytp-progress-bar:hover #timelens,
 .ytp-progress-bar-container.ytp-drag #timelens {
-  opacity: 0.8;
+  opacity: 0.9;
   display: block;
 }
 .ytp-tooltip.ytp-preview {
